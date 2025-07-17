bf0f5836c0b9070674ae36770165c1e323c2746c
Sharypov Konstantin <shariktlt@gmail.com>
2025-07-17 19:53:31 +0300

    Актуализация

--- src/e4/utils/Part3_DateTime.java
+++ src/e4/utils/Part3_DateTime.java
@@ -2,6 +2,14 @@ package e4.utils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalField;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
@@ -21,10 +29,26 @@ public class Part3_DateTime {
         System.out.println(simpleDateFormat.parse("2000-05-22"));
 
 
+        Instant now = Instant.now();
+        now.get(ChronoField.DAY_OF_YEAR);
+        now.get(ChronoField.DAY_OF_MONTH);
+        now.get(ChronoField.HOUR_OF_DAY);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        System.out.println(formatter.format(now));
+        System.out.println(formatter.parse("2025-01-01"));
+
+
         GregorianCalendar calendar = new GregorianCalendar(); //Грегорианский и юлианский календари с правилами летнего времени и т.д.
 
         calendar.setTimeZone(TimeZone.getTimeZone("Europe/London"));
         calendar.add(GregorianCalendar.MINUTE, 30);
         System.out.printf("Время через пол часа: %s:%s%n", calendar.get(GregorianCalendar.HOUR_OF_DAY), calendar.get(GregorianCalendar.MINUTE));
+
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        ZonedDateTime londonTime = zonedDateTime.withZoneSameInstant(ZoneId.of("Europe/London"));
+        ZonedDateTime halfHourAhead = londonTime.plusMinutes(30);
+        System.out.printf("Время через пол часа2: %s:%s%n", halfHourAhead.getHour(), halfHourAhead.getMinute());
+
     }
 }
